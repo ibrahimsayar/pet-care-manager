4dd063330b1b0dc47920905294c0b85de22448b7
ibrahim.sayar <ibrahim.sayar@naylalabs.com>
2024-01-11 21:03:35 +0300

    chore: rename surname column to last name in users table

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -17,6 +18,11 @@ class UserFactory extends Factory
     protected static ?string $password;
 
     /**
+     * @var string
+     */
+    protected $model = User::class;
+
+    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
@@ -29,6 +35,8 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'last_name' => fake()->lastName(),
         ];
     }
 
