29d65a65dae83938cf8ee08f4b4180d0796190df
ibrahim.sayar <ibrahim.sayar@naylalabs.com>
2024-01-16 20:40:59 +0300

    feat: add exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,11 @@
 
 namespace App\Exceptions;
 
+use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -26,5 +30,45 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * @param $request
+     * @param  Exception|Throwable  $e
+     * @return Response|JsonResponse|RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * @throws Throwable
+     */
+    public function render(
+        $request,
+        Exception|Throwable $e
+    ): Response|JsonResponse|RedirectResponse|\Symfony\Component\HttpFoundation\Response {
+        if ($request->expectsJson()) {
+            return $this->response($e);
+        }
+
+        return parent::render($request, $e);
+    }
+
+    /**
+     * @param  Exception  $exception
+     * @return JsonResponse
+     */
+    protected function response(Exception $exception): JsonResponse
+    {
+        $statusCode = $this->getStatusCode($exception);
+
+        return new JsonResponse([
+            'status' => false,
+            'message' => $exception->getMessage(),
+        ], $statusCode);
+    }
+
+    /**
+     * @param  Exception  $exception
+     * @return int
+     */
+    protected function getStatusCode(Exception $exception): int
+    {
+        return method_exists($exception, 'getStatusCode') ? $exception->getStatusCode() : 500;
     }
 }
